126a09c8382b641545f5143262f820878e7902b0
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-19 15:03:55 +0200

    setup folders | start palindrome ex

---
+++ palindromo/assets/js/main.js
@@ -0,0 +1,12 @@
+//Chiedere all’utente di inserire una parola 
+//Creare una funzione per capire se la parola inserita è palindroma
+
+/* STRUMENTI
+- prompt();
+- String.split();
+- string.reverse();
+- string.join();
+- if
+*/
+
+//chiedo all'utent un nome palindromo
